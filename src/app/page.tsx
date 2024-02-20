0cf7f87697f24d261632304032247d0eb5698c80
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-02-20 20:32:58 -0300

    feat: 🎸 Product items carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,42 @@
 import Product from '@/app/components/product'
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from '@/app/components/ui/carousel'
 
 export default function Home() {
     return (
-        <main className="container mt-10 flex cursor-pointer place-items-center gap-12 rounded-lg p-1">
-            <Product />
-            <Product />
-            <Product />
+        <main className="container mt-10 w-full">
+            <Carousel
+                opts={{
+                    align: 'start',
+                }}
+                className="w-full"
+            >
+                <CarouselContent>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                        <Product />
+                    </CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                        <Product />
+                    </CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                        <Product />
+                    </CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                        <Product />
+                    </CarouselItem>
+                    <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+                        <Product />
+                    </CarouselItem>
+                </CarouselContent>
+
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
         </main>
     )
 }
